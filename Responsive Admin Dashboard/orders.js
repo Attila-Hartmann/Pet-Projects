e331b019765cb90e82e5ca66a548d0bcfd708408
Attila Hartmann <hartmann.attila88@gmail.com>
2023-01-27 09:47:20 +0100

    Dashboard added

---
+++ Responsive Admin Dashboard/orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        name: 'Foldable Mini Drone',
+        number: '85361',
+        status: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        name: 'Larvender KF102 Drone',
+        number: '36378',
+        status: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        name: 'Ruko F11 Pro Drone',
+        number: '49347',
+        status: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        name: 'Camera Pro Drone',
+        number: '96996',
+        status: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        name: 'GPS 4K Drone',
+        number: '22821',
+        status: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        name: 'DJI Air 2S',
+        number: '81475',
+        status: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        name: 'Lozenge Drone',
+        number: '00482',
+        status: 'Paid',
+        shipping: 'Delivered'
+    }
+]
